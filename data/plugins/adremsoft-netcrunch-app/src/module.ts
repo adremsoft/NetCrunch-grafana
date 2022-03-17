be39df4b8d8da9d338e3318594e27b8dd32c4ed9
Marcin Banasik <marcin@adrem.com.pl>
2022-03-17 14:54:02 +0100

    Cleanup

--- data/plugins/adremsoft-netcrunch-app/src/module.ts
+++ data/plugins/adremsoft-netcrunch-app/src/module.ts
@@ -1,7 +1,3 @@
 import { AppPlugin } from '@grafana/data';
 
-import { ExampleConfigCtrl } from './legacy/config';
-
-export { ExampleConfigCtrl as ConfigCtrl };
-
 export const plugin = new AppPlugin<{}>();
