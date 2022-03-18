3c0bc94486309ac1ae6aab9ee8210ac41372a292
Marcin Banasik <marcin@adrem.com.pl>
2022-03-18 09:55:03 +0100

    Missing image

--- data/plugins/adremsoft-netcrunch-app/build.cmd
+++ data/plugins/adremsoft-netcrunch-app/build.cmd
@@ -4,5 +4,7 @@ call yarn build
 mkdir dist\datasource\css
 copy src\datasource\css\query.editor.css dist\datasource\css
 
+copy src\images\load_big.gif dist\images
+
 copy src\datasource\services\netCrunchAPI\adrem\*min.js dist\datasource\services\netCrunchAPI\adrem\
 
