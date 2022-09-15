b3de38748f351258465955507d62c9f22dd189fc
Tomasz Kunicki <tkunicki@adrem.com.pl>
2022-09-15 19:58:37 +0200

    Try to avoid mangling `sortNodesByNameAndAddress`

--- data/plugins/adremsoft-netcrunch-app/src/datasource/services/netCrunchAPI/netCrunchConnection/networkData/nodesOperations.js
+++ data/plugins/adremsoft-netcrunch-app/src/datasource/services/netCrunchAPI/netCrunchConnection/networkData/nodesOperations.js
@@ -12,54 +12,59 @@ const THREAD_WORKER_NODES_NUMBER = 1000;
 
 let webWorkerSingleton = null;
 
-function sortNodesByNameAndAddress(nodes) {
+const tasks = {
+  sortNodesByNameAndAddress(nodes) {
+    function compareAddressIP(addressOne, addressTwo) {
+      const
+        addressOneItems = addressOne.split('.'),
+        addressTwoItems = addressTwo.split('.');
 
-  function compareAddressIP(addressOne, addressTwo) {
-    const
-      addressOneItems = addressOne.split('.'),
-      addressTwoItems = addressTwo.split('.');
-
-    for (let i = 0, n = Math.max(addressOneItems.length, addressTwoItems.length); i < n; i += 1) {
-      if (addressOneItems[i] !== addressTwoItems[i]) {
-        return (addressOneItems[i] < addressTwoItems[i]) ? -1 : 1;
+      for (let i = 0, n = Math.max(addressOneItems.length, addressTwoItems.length); i < n; i += 1) {
+        if (addressOneItems[i] !== addressTwoItems[i]) {
+          return (addressOneItems[i] < addressTwoItems[i]) ? -1 : 1;
+        }
       }
+      return 0;
     }
-    return 0;
-  }
 
-  function getNodeProperty(node, propertyName) {
-    return ((node != null) && (node[propertyName] != null)) ? node[propertyName] : '';
-  }
-
-  function compareNodeData(nodeA, nodeB) {
-    const
-      nodeAName = getNodeProperty(nodeA, 'name').toLowerCase(),
-      nodeBName = getNodeProperty(nodeB, 'name').toLowerCase(),
-      nodeAAddress = getNodeProperty(nodeA, 'address'),
-      nodeBAddress = getNodeProperty(nodeB, 'address');
-    let result = 0;
-
-    if ((nodeAName !== '') && (nodeBName !== '')) {
-      result = nodeAName.localeCompare(nodeBName);
-    } else if ((nodeAName === '') && (nodeBName === '')) {
-      result = compareAddressIP(nodeAAddress, nodeBAddress);
-    } else {
-      if (nodeAName !== '') { result = -1; }
-      if (nodeBName !== '') { result = 1; }
+    function getNodeProperty(node, propertyName) {
+      return ((node != null) && (node[propertyName] != null)) ? node[propertyName] : '';
     }
-    return result;
+
+    function compareNodeData(nodeA, nodeB) {
+      const
+        nodeAName = getNodeProperty(nodeA, 'name').toLowerCase(),
+        nodeBName = getNodeProperty(nodeB, 'name').toLowerCase(),
+        nodeAAddress = getNodeProperty(nodeA, 'address'),
+        nodeBAddress = getNodeProperty(nodeB, 'address');
+      let result = 0;
+
+      if ((nodeAName !== '') && (nodeBName !== '')) {
+        result = nodeAName.localeCompare(nodeBName);
+      } else if ((nodeAName === '') && (nodeBName === '')) {
+        result = compareAddressIP(nodeAAddress, nodeBAddress);
+      } else {
+        if (nodeAName !== '') {
+          result = -1;
+        }
+        if (nodeBName !== '') {
+          result = 1;
+        }
+      }
+      return result;
+    }
+
+    return nodes
+      .filter(node => ((node != null) && ((node.id != null) && ((node.name != null) && (node.address != null)))))
+      .sort(compareNodeData);
+
   }
-
-  return nodes
-    .filter(node => ((node != null) && ((node.id != null) && ((node.name != null) && (node.address != null)))))
-    .sort(compareNodeData);
-
-}
+};
 
 function getWebWorker() {
   if (webWorkerSingleton == null) {
     const workerBuilder = AdremWebWorker.webWorkerBuilder();
-    workerBuilder.addFunctionCode(sortNodesByNameAndAddress, true);
+    workerBuilder.addFunctionCode(tasks.sortNodesByNameAndAddress, true);
     webWorkerSingleton = workerBuilder.getWebWorker();
   }
   return webWorkerSingleton;
@@ -70,7 +75,7 @@ class NetCrunchNodesOperations {
   static asyncSortByNameAndAddress(nodes = []) {
     return new Promise((resolve) => {
       if (nodes.length < THREAD_WORKER_NODES_NUMBER) {
-        const result = sortNodesByNameAndAddress(nodes);
+        const result = tasks.sortNodesByNameAndAddress(nodes);
         resolve(result);
       } else {
         const nodesRemoteBuffer = [];
